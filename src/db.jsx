c4bb8127a5cfab9837fb8cd15ad6d66afe5931f2
unknown <mjsa.sanches25@gmail.com>
2023-12-27 14:05:24 -0300

    first commit

--- src/db.jsx
+++ src/db.jsx
@@ -1,13 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
+const apiKey = import.meta.env.REACT_APP_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_APP_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCXdNd1V7fdxypW3FUuZZG4XhRyssRJM3k",
-  authDomain: "owlnutri.firebaseapp.com",
-  projectId: "owlnutri",
-  storageBucket: "owlnutri.appspot.com",
-  messagingSenderId: "242458868060",
-  appId: "1:242458868060:web:85a7cb90d38cea8f55e999",
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
